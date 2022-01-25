1b37c3ed71cce118a3f57a39333ed436be3431da
sonal-511 <sonalgupta115@gmail.com>
2022-01-26 00:32:46 +0530

    finding merge conflict

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ print("i am second commit")
 
 print("third commit and did changes here")
 
-def funct(a,b):
-    sum = a+b
-    return sum
+def funct(a,big):
+    sum = a+big/2
+    return sum/2
 print(funct(7,9))
