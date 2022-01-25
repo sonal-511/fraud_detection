44554c986622f73a30458535f1484a5ce7dd0230
sonal-511 <sonalgupta115@gmail.com>
2022-01-26 01:03:12 +0530

    change in variabel name

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ print("i am second commit")
 
 print("third commit and did changes here")
 
-def funct(a,b):
-    sum = a+b
-    return sum
+def funct(a,b_model2):
+    sum = a+b_model2/9
+    return sum+10
 print(funct(7,9))
